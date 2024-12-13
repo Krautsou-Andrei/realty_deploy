ebb999f7c1d1837aebeaa372adae16261872cd1e
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-12-13 15:08:26 +0300

    feat; file project for host

---
+++ theme/al-assistant.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: al-assistant
+*/
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Чат-бот на PHP</title>
+</head>
+
+<body>
+   
+</body>
+
+</html>
