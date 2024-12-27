60fe103bd1269668e37caaeefa30d947b53a9abd
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2024-12-27 18:22:12 +0300

    fix: search category city by slug

--- theme/inc/lib/get_params_filter_catalog.php
+++ theme/inc/lib/get_params_filter_catalog.php
@@ -1,7 +1,9 @@
 <?php
 require_once get_template_directory() . '/inc/lib/get_names_children_categories.php';
 require_once get_template_directory() . '/inc/lib/get_link_page_by_template.php';
+require_once get_template_directory() . '/inc/lib/get_transliterate.php';
 require_once get_template_directory() . '/inc/lib/search_id_category_by_name.php';
+require_once get_template_directory() . '/inc/lib/search_id_category_by_slug.php';
 require_once get_template_directory() . '/inc/lib/search_id_page_by_name.php';
 require_once get_template_directory() . '/inc/enums/categories_name.php';
 require_once get_template_directory() . '/inc/enums/default_enum.php';
@@ -11,9 +13,9 @@ require_once get_template_directory() . '/inc/enums/template_name.php';
 function get_params_filter_catalog()
 {
     global  $names_default_cities;
-  
+
     $filter_region = isset($_GET['region']) ? $_GET['region'] : DEFAULT_ENUM::DEFAULT_FILTER_REGION;
-  
+
     $dafault_city = $names_default_cities[$filter_region] ?? DEFAULT_ENUM::DEFAULT_FILTER_CITY;
     $filter_city = isset($_GET['city']) ? $_GET['city'] : $dafault_city;
 
@@ -35,9 +37,9 @@ function get_params_filter_catalog()
     $filter_area_do = isset($filter_area_array[1]) ? $filter_area_array[1] : '';
 
     $regions_parent_category_id  = search_id_category_by_name(CATEGORIES_NAME::REGIONS);
-
     $regions_names = !empty($regions_parent_category_id) ? get_names_children_categories($regions_parent_category_id) : [];
-    $cities_parent_category_id  = search_id_category_by_name($filter_region);
+
+    $cities_parent_category_id  = search_id_category_by_slug(trim(get_transliterate($filter_region)));
     $cities_names = !empty($cities_parent_category_id) ? get_names_children_categories($cities_parent_category_id) : [];
 
     $rooms_parent_category_id = search_id_category_by_name(CATEGORIES_NAME::ROOMS);
@@ -69,6 +71,7 @@ function get_params_filter_catalog()
     );
 
     $gk_city = get_posts($args_gk_city);
+
     $max_price = 2;
 
     foreach ($gk_city as $gk_id) {
